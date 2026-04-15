519a589262ced1f503f746554133a6ce4c456326
Nick Beaugeard <nick@releasedgroup.com>
2026-04-15 15:21:07 +1000

    Fix updateProfile registration after rebase

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,11 +3,9 @@ import { registerHealthFunction } from './functions/health.js'
 import { registerUpdateProfileFunction } from './functions/update-profile.js'
 import { registerUsersByHandleMirrorFunction } from './functions/users-by-handle-mirror.js'
 import { registerAuthMeFunction } from './functions/me.js'
-import { registerUpdateProfileFunction } from './functions/update-profile.js'
 
 registerGetUserFunction()
 registerHealthFunction()
 registerUpdateProfileFunction()
 registerUsersByHandleMirrorFunction()
 registerAuthMeFunction()
-registerUpdateProfileFunction()
